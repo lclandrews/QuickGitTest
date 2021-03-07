349336f09e08bd4d8e21fc6d3ed53eab25b5fb8c
lclandrews <lclandrews@gmail.com>
2021-03-07 13:48:30 +0000

    - Added something cool to look with mouse script

--- Assets/Scripts/LookWithMouse.cs
+++ Assets/Scripts/LookWithMouse.cs
@@ -53,4 +53,9 @@ public class LookWithMouse : MonoBehaviour
 
         playerBody.Rotate(Vector3.up * mouseX);
     }
+
+    void SomeNewTestFunction()
+    {
+        // Do something cool here
+    }
 }
